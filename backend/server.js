01be794e8a93947fdc8a0f52a13c354f8cef75ed
Yasini V <yasinivijayakumar021@gmail.com>
2026-05-14 15:13:34 +0530

    Final push

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,8 @@ app.use("/stories", storyRoutes);
 app.use("/users", userRoutes);
 
 
-app.listen(4000, () => {
-  console.log("Server running on port 4000");
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
